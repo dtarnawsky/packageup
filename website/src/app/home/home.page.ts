ae2c43cda3f511c3a5eeb759d2558a1223ae5961
damian.ionic <damian@ionic.io>
2023-12-02 12:29:16 -0800

    feat: improve

--- website/src/app/home/home.page.ts
+++ website/src/app/home/home.page.ts
@@ -1,29 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent, IonText, IonButton } from "@ionic/angular/standalone";
+import { IonContent, IonText, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { logoGithub, copyOutline } from 'ionicons/icons';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
     standalone: true,
-    imports: [IonContent, IonText, IonButton, IonContent, IonText],
+    imports: [IonContent, IonText, IonButton, IonContent, IonText, IonIcon],
 })
 export class HomePage implements OnInit {
     constructor() {
-
+        addIcons({ logoGithub, copyOutline });
     }
 
     ngOnInit(): void {
-        if (window.location.search.includes('?app')) {
-            var ua = navigator.userAgent.toLowerCase();
-            var isAndroid = ua.indexOf("android") > -1;
-            var isiOS = ua.indexOf("iphone") > -1;
-            if (isAndroid) {
-                window.location.href = "https://play.google.com/store/apps/details?id=nexus.concepts.dust";
-            }
-            if (isiOS) {
-                window.location.href = "https://apps.apple.com/us/app/dust-a-guide-for-burners/id6456943178";
-            }
-        }
+    }
+
+    copy() {
+        const copyText: any = document.getElementById("cmd");
+        if (!copyText) return;
+        // Select the text field
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); // For mobile devices
+
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(copyText.value);
     }
 }
