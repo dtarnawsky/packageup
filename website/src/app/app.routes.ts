55995ef0f55177de9c4b5ac2f8e7c9a628f8a8e8
damian.ionic <damian@ionic.io>
2023-12-03 11:04:45 -0800

    fix: cleanup

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -2,13 +2,8 @@ import {  Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'index',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },  
-  {
     path: '',
-    redirectTo: document.location.hostname.startsWith('edit') ? 'all' : 'index',
-    pathMatch: 'full',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   }
 ];
 
